ef981565c09426bb48855bc5ef8c7afb37f8fcba
Amit Kumar <amitkumar790194@gmail.com>
2024-06-16 08:31:04 +0530

    code Editor setup

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,12 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
+import Footer from './Footer'
 
 export const Landing = () => {
   return (
-    <div>Landing</div>
+    <>
+        <Box bgGradient='linear(to-r, pink.500, red.500, yellow.500)' w={"100%"} h={"0.2rem"}/>
+        <Footer/>
+    </>
   )
 }
